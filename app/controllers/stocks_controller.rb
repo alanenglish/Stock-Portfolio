3ea93654a1f5f14f5b9656a24da6b4b97c7eb11c
Alan English <aenglish@manorhillcapital.com>
2018-12-26 23:08:34 -0600

    handle errors on invalid ticker and empty ticker submission

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        render 'users/portfolio'
+      else
+        flash[:danger] = "You have entered an invalid stock ticker."
+        redirect_to portfolio_path
+      end
+    else
+      flash[:danger] = "Please submit a valid stock ticker."
+      redirect_to portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,10 @@
 class Stock < ApplicationRecord
   def self.new_from_lookup(ticker)
-    stock = StockQuote::Stock.quote(ticker)
-    new(company: stock.company_name, ticker: stock.symbol, last_price: stock.latest_price)
+    begin
+      stock = StockQuote::Stock.quote(ticker)
+      new(company: stock.company_name, ticker: stock.symbol, last_price: stock.latest_price)
+    rescue Exception => e
+      return nil
+    end
   end
 end
