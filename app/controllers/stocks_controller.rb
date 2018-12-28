9e59ecd1bddd34c573b4b6192c94e1827f4472fa
Alan English <aenglish@manorhillcapital.com>
2018-12-27 22:24:31 -0600

    add AJAX to stock lookup form to avoid full page refresh, update controller, create result partial and result js file to attach response to result div

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,7 +3,9 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_from_lookup(params[:stock])
       if @stock
-        render 'users/portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
         flash[:danger] = "You have entered an invalid stock ticker."
         redirect_to portfolio_path
